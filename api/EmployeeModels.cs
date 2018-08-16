6406bb5c0e89a1ac5b2a578697602fa3cafb90b5
nenew121 <33708352+nenew121@users.noreply.github.com>
2018-08-16 14:36:39 +0700

    Add files via upload

---
+++ api/EmployeeModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ESSLineBotAPI.Models
+{
+    public class EmployeeModels
+    {
+        public string EmpID { get; set; }
+        public string EmpCode { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string TitleEng { get; set; }
+        public string FirstNameEng { get; set; }
+        public string MiddleNameEng { get; set; }
+        public string LastNameEng { get; set; }
+        public string NickNameEng { get; set; }
+        public string FullName { get; set; }
+        public string FullNameEng { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
---
+++ api/ReadText.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myfile = fopen("C:/Users/com/Dropbox/แอพ/Heroku/cherry-pie-82107/ess-linebot/Text.txt", "r") or die("Unable to open file!");
+$URL = fgets($myfile).'/aip/EmpID?OrgID=12412124s124___'.date("Y-m-d H:i:s");
+echo $URL;
+
+
+fclose($myfile);
+?>
+
+</body>
+</html>
